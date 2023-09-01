72ea1b6d28e01e07289f914f05f9920b1cb4e9ec
Luke Salazar <lucassy07@outlook.com>
2023-09-01 18:08:10 -0500

    Main meeting, Features divided

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Landing from './components/landing/Landing';
+import Login from './components/user/login/Login';
+import Header from './components/partials/header/Header';
+import Footer from './components/partials/footer/Footer';
 
 function App() {
   return (
     <>
+      <Header></Header>
       <Routes>
         <Route path='/' element={ <Landing /> } />
+        <Route path='/login' element={ <Login /> } />
       </Routes>
+      <Footer></Footer>
     </>
   );
 }
