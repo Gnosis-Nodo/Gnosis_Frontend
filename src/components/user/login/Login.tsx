c955eddeaf18a2e959d57309b4a848f5c1512845
jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-09-04 18:33:15 -0500

    Styling login page

--- src/components/user/login/Login.tsx
+++ src/components/user/login/Login.tsx
@@ -1,6 +1,5 @@
 /*
   This example requires some changes to your config:
-  
   ```
   // tailwind.config.js
   module.exports = {
@@ -12,29 +11,33 @@
   }
   ```
 */
+import LogoGnosis from '../../../assets/imgs/logo-gnosis.svg';
+import './Login.scss';
+
 export default function Login() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+            className="mx-auto h-28 w-auto"
+            src={ LogoGnosis }
+            alt="logo-gnosis"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+          <h2 className="mt-1 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+            Gnosis
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="email" className="block text-2x1 font-bold leading-6 text-gray-900">
                 Email address
               </label>
               <div className="mt-2">
                 <input
+                placeholder='email@adress.com'
                   id="email"
                   name="email"
                   type="email"
@@ -47,17 +50,13 @@ export default function Login() {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="password" className="block text-2x1 font-bold leading-6 text-gray-900">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
+                  placeholder='password'
                   id="password"
                   name="password"
                   type="password"
@@ -67,23 +66,31 @@ export default function Login() {
                 />
               </div>
             </div>
-
+            <label htmlFor="checkbox-reminder">
+              <input id='checkbox' type="checkbox" />Remind me
+            </label>
+            
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-[#000A61] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
+          <div className="text-sm">
+            <a href="" className="text-1 font-medium text-[#000000] hover:text-indigo-500">
+              Forgot password?
             </a>
+          </div>
+          <p className="mt-6 text-center text-sm text-gray-500">
+            Sign in with your social media{' '} <br />
+
           </p>
+          <a href="" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            Start a 14 day free trial
+          </a>
         </div>
       </div>
     </>
