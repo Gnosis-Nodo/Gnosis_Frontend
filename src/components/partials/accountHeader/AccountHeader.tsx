27132f08325cc040a602dc16f00d887970cb550a
jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-09-05 14:16:44 -0500

    Adding AccountHeader HTML structure

---
+++ src/components/partials/accountHeader/AccountHeader.tsx
@@ -0,0 +1,13 @@
+import './AccountHeader.scss';
+
+export default function AccountHeader() {
+    return (
+      <>
+      <nav>
+        <p>Settings</p>
+        <p>Statistics</p>
+        <p>Saved</p>
+      </nav>
+      </>
+    )
+  }
