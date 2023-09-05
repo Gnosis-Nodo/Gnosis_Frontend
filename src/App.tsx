b220c03ca428e73da513b31887848e37af2554f9
lsalinasp <117488134+lsalinasp@users.noreply.github.com>
2023-09-05 03:19:38 +0000

    Revert "Revert "Lula b""
    This reverts commit 9daeb6436ebc4cb2b7bf4f57353b549c168f6ab8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import ScrollToTop from './components/behaviour/ScrollToTop';
 import Landing from './components/landing/Landing';
 import Login from './components/user/login/Login';
 import Header from './components/partials/header/Header';
 import Footer from './components/partials/footer/Footer';
+<<<<<<< HEAD
 import Content from './components/content/Content';
+=======
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Pricing from './components/pricing/Pricing';
+>>>>>>> parent of 9daeb64 (Revert "Lula b")
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <ScrollToTop />
+      <Header />
       <Routes>
         <Route path='/' element={ <Landing /> } />
         <Route path='/login' element={ <Login /> } />
+<<<<<<< HEAD
         <Route path='/content' element={ <Content /> } />
+=======
+        <Route path='/home' element={ <Home /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/pricing' element={ <Pricing /> } />
+>>>>>>> parent of 9daeb64 (Revert "Lula b")
       </Routes>
-      <Footer></Footer>
+      <Footer/>
     </>
   );
 }
