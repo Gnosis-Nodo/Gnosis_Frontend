3b6962f3e4a282891df30a62ea3ade9d8b7436e4
lsalinasp <117488134+lsalinasp@users.noreply.github.com>
2023-09-05 03:26:12 +0000

    MERGE LUK AND LU

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,10 @@ import Landing from './components/landing/Landing';
 import Login from './components/user/login/Login';
 import Header from './components/partials/header/Header';
 import Footer from './components/partials/footer/Footer';
-<<<<<<< HEAD
 import Content from './components/content/Content';
-=======
 import Home from './components/home/Home';
-import About from './components/about/About';
 import Pricing from './components/pricing/Pricing';
->>>>>>> parent of 9daeb64 (Revert "Lula b")
+import About from './components/about/About';
 
 function App() {
   return (
@@ -21,13 +18,10 @@ function App() {
       <Routes>
         <Route path='/' element={ <Landing /> } />
         <Route path='/login' element={ <Login /> } />
-<<<<<<< HEAD
         <Route path='/content' element={ <Content /> } />
-=======
         <Route path='/home' element={ <Home /> } />
-        <Route path='/about' element={ <About /> } />
         <Route path='/pricing' element={ <Pricing /> } />
->>>>>>> parent of 9daeb64 (Revert "Lula b")
+        <Route path='/about' element={ <About /> } />
       </Routes>
       <Footer/>
     </>
