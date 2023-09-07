b3f09967801064cf0e6dd6fd69994cb70bb00413
jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-09-06 22:25:47 -0500

    Adding phtos for section page, building style for section

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,59 +1,130 @@
+import Coding from '../../assets/svg/coding-data-development-svg-com.svg';
+import Audio from '../../assets/svg/audio-guide-svg.svg';
+import Reading from '../../assets/svg/reading-logo-svg.svg';
+import Video from '../../assets/svg/video-library-svg.svg';
+import Python from '../../assets/svg/python-logo-only.svg';
+import Locked from '../../assets/svg/lock-logo.svg';
+import JS from '../../assets/svg/javascript-logo.svg';
+import Java from '../../assets/svg/java-logo.svg';
+import HTML from '../../assets/svg/html-5-logo.svg';
 import './Section.scss';
 
 export default function Section() {
   return (
     <>
-    <header className='products'>
-      <div className='coding'>
-        <img src="" alt="coding-logo" />
-        <p>Coding</p>
-      </div>
-      <div className='audio'>
-        <img src="" alt="audio-logo" />
-        <p>Audio</p>
-      </div>
-      <div className='reading'>
-        <img src="" alt="reading-logo" />
-        <p>Reading</p>
-      </div>
-            <div className='video'>
-            <img src="" alt="video-logo" />
-              <p>Video</p>
-            </div>
-            <p>Discover different ways to learn </p>
-        </header>
-    <main className='front'>
-        
-        <nav>
-            <div>
-                <input type="text" />
-                <img src="" alt="search-icon" />
-            </div>
-            <select name="topics" id="topics">
-              <option value="0">Selection</option>
-              <option value="1">Coding</option>
-              <option value="2">Audio</option>
-              <option value="3">Reading</option>
-              <option value="4">Video</option>
-            </select>
-        </nav>
-        <section>
+      <header className='products'>
+        <div className='products-info'>
+          <div className='coding'>
+            <img src={ Coding } alt="coding-logo" />
+            <p>Coding</p>
+          </div>
+          <div className='audio'>
+            <img src={ Audio } alt="audio-logo" />
+            <p>Audio</p>
+          </div>
+          <div className='reading'>
+            <img src={ Reading } alt="reading-logo" />
+            <p>Reading</p>
+          </div>
+          <div className='video'>
+            <img src={ Video } alt="video-logo" />
+            <p>Video</p>
+          </div>
+        </div>
+        <p>Discover 4 different ways to learn, easy and, fun. We have more than courses and concepts to study </p>
+      </header>
+      <nav className='searching-section'>
+        <div className='searching-navbar'>
+          <input placeholder='Search...'
+            id="search"
+            name="search"
+            type="text"
+            autoComplete="last-name"
+            required
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-4 placeholder: px-3 outline-none"/>
+        </div>
+        <select name="topics" id="topics">
+          <option value="0">Selection</option>
+          <option value="1">Coding</option>
+          <option value="2">Audio</option>
+          <option value="3">Reading</option>
+          <option value="4">Video</option>  
+        </select>
+      </nav>
+      <main>
+        <div className='card-display'>
           <div className='card-container'>
-            <div className='card'>
-              <img src="" alt="card-logo" />
-              <p>Title</p>
+            <div className='card-heading'>
+              <img src= { JS } alt="js-logo" />
+              <p>JavaScript</p>
             </div>
             <div className='progress'>
-              <span className='portion'>4/100</span>
+              <span className='portion'>30/100 Exercises</span>
               <span className='amount-concepts'>28 concepts</span>
             </div>
             <div className='progress-bar'>
-              <progress id='bar' value="4" max="100">4 %</progress>
+              <progress id='bar' value="30" max="100">30%</progress>
               <p className='last-seen'>Last seen 2 days ago</p>
             </div>
           </div>
-        </section>
-    </main>
+          <div className='card-container'>
+            <div className='card-heading'>
+              <img src={ Python } alt="python-logo" />
+              <p>Python</p>
+            </div>
+            <div className='progress'>
+              <span className='portion'>4/100 Exercises</span>
+              <span className='amount-concepts'>32 concepts</span>
+            </div>
+            <div className='progress-bar'>
+              <progress id='bar' value="4" max="100">4 %</progress>
+              <p className='last-seen'>Last seen 1 hour ago</p>
+            </div>
+          </div>
+          <div className='card-container'>
+            <div className='card-heading'>
+              <img src={ Java } alt="java-logo" />
+              <p>Java</p>
+            </div>
+            <div className='progress'>
+              <span className='portion'>16/130 Exercises</span>
+              <span className='amount-concepts'>50 concepts</span>
+            </div>
+            <div className='progress-bar'>
+              <progress id='bar' value="12" max="100">12%</progress>
+              <p className='last-seen'>Last seen 2 days ago</p>
+            </div>
+          </div>
+          <div className='card-container'>
+            <div className='card-heading'>
+              <img src={ HTML } alt="html-logo" />
+              <p>HTML 5</p>
+            </div>
+            <div className='progress'>
+              <span className='portion'>16/20 Exercises</span>
+              <span className='amount-concepts'>12 concepts</span>
+            </div>
+            <div className='progress-bar'>
+              <progress id='bar' value="80" max="100">80%</progress>
+              <p className='last-seen'>Last seen 2 days ago</p>
+            </div>
+          </div>
+          <div className='card-container'>
+            <div className='card-heading'>
+              <img src={ Locked } alt="lock-logo" />
+              <p>Spring</p>
+            </div>
+            <div className='progress'>
+              <span className='portion'>0/30 Exercises</span>
+              <span className='amount-concepts'>18 concepts</span>
+            </div>
+            <div className='progress-bar'>
+              <progress id='bar' value="0" max="100">0%</progress>
+              <p className='last-seen'>Locked</p>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
--- src/components/user/account/Account.tsx
+++ src/components/user/account/Account.tsx
@@ -1,4 +1,6 @@
 import './Account.scss';
+import Avatar from '../../../assets/svg/avatar-bad-breaking-svg.svg';
+import Upload from '../../../assets/svg/upload-logo-svg.svg';
 import AccountHeader from '../../../components/partials/accountHeader/AccountHeader';
 
 export default function Account() {
@@ -13,7 +15,7 @@ export default function Account() {
                 <form className="flex items-center space-y-4 space-x-4" action="#" method="POST">
                     <div>
                         <div className="flex items-center justify-between">
-                            <label htmlFor="name" className="block text-2x1 font-bold leading-6 text-gray-900">
+                            <label htmlFor="name" className="block text-2x1 leading-6 text-gray-900">
                             Name
                             </label>
                         </div>
@@ -40,7 +42,7 @@ export default function Account() {
                                 placeholder='last-name'
                                 id="last-name"
                                 name="last-name"
-                                type="last-name"
+                                type="text"
                                 autoComplete="last-name"
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder: px-3 outline-none"
@@ -49,8 +51,8 @@ export default function Account() {
                         </div>
                         <div>
                         <div className="flex items-center justify-between">
-                            <label htmlFor="name" className="block text-2x1 font-bold leading-6 text-gray-900">
-                            Name
+                            <label htmlFor="name" className="block text-2x1 leading-6 text-gray-900">
+                            Bio
                             </label>
                         </div>
                         <div className="my-1">
@@ -69,8 +71,8 @@ export default function Account() {
                 </div>
                 <h3>Photo</h3>
                 <div className='photo-container'>
-                    <img src="" alt="user" />
-                    <button>Upload <img src="" alt="" /></button>
+                    <img src={ Avatar } alt="user" />
+                    <button>Upload <img src={ Upload } alt="" /></button>
                 </div>
                 <h3>Email</h3>
                 <div className='email-container'>
