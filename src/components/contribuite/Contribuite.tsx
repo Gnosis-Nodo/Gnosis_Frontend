08f1691a8e0de97d4692f9973a9ec323ea2a2923
alexgarpul <alexandergarciaclei5b@gmail.com>
2023-09-08 10:31:07 -0500

    contributors component is added

--- src/components/contribuite/Contribuite.tsx
+++ src/components/contribuite/Contribuite.tsx
@@ -1,4 +1,5 @@
 import Card from './Card';
+import Constributors from './Contributors';
 import Message from './Message';
 
 const Contribuite = () => {
@@ -11,8 +12,9 @@ const Contribuite = () => {
             <p className="selector mr-4 ml-12 hover:bg-[#000A61] text-black hover:text-white">Empezar</p>
             <p className="selector hover:bg-[#000A61] text-black hover:text-white">Contribuidores</p>
         </div>
-            <div className='container w-[1512px] h-[1634px] flex flex-wrap gap-[50px]  gap-y-0 items-center content-start justify-center flex-row mt-[15px] '>
+            <div className='container w-[1512px] h-[1634px] flex flex-wrap gap-[50px]  gap-y-0 items-center content-start justify-center flex-row  '>
                 <Card/>
+                <Constributors/>
             </div>
         </>
     )
--- src/components/contribuite/Contributors.tsx
+++ src/components/contribuite/Contributors.tsx
@@ -1,7 +1,154 @@
+import Rectangle from '../../assets/imgs/Rectangle.png'
+import Star from '../../assets/imgs/Star 1.png'
+
 const Constributors = () => {
+    const text = {
+        descrirption:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.",
+      };
     return(
-        <div>
-            <p></p>
+        <div className='flex item-center flex-col grap-y-0'>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>98</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>75</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>74</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>69</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>65</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>59</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>55</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>50</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>45</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
+        <div className='flex w-[1130px] h-[75px] bg-[#D9D9D980] rounded-[27px] grap-[2px] bg-opacity-50 shadow-md flex-shrink-0 mt-[67px] justify-between'>
+            <div className='m-[8px] ml-[20px] mr-[20px]'>
+                <img className='w-[62px] h-[59px] flex-shrink-0 rounded-[100px] border border-solid ' src={Rectangle} alt="" />
+            </div>
+            <div className='flex flex-col mr-[260px]'>
+                <h1 className='text-[var(--second-font, #000)] font-barlow text-[35px] italic font-semibold'>Lorem</h1>
+                <p className='w-[623px] h-[19px]  mb-[1px] text-[12px]'>{text.descrirption}</p>
+            </div>
+            <div className='flex w-[140px] h-[51px] flex-shrink-0 rounded-[27px] bg-[#000A61] shadow-md justify-center my-[auto] mr-[15px]'>
+                <h1 className='text-[30px] text-[white] my-auto ml-[5px]'>30</h1>
+                <img className='w-[30px] h-[30px] flex-shrink-0 my-[auto] ml-[25px]' src={Star} alt="?" />
+            </div>
+        </div>
+
         </div>
     )
 }
